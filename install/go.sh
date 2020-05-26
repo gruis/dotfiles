89385b591807d20b939e4624f57ecda41229053f
Caleb Crane <github@simulacre.org>
2020-05-26 20:18:58 +0900

    build essential for Make and go

--- install/go.sh
+++ install/go.sh
@@ -5,3 +5,5 @@ set -e
 sudo wget -q -c https://dl.google.com/go/go1.13.11.linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local
 echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee /etc/profile.d/gopath.sh
 mkdir -p ~/go
+
+sudo apt-get -y install build-essential
