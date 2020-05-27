d56046588623f350635feec84f685f3ba64aa4e1
Caleb Crane <github@simulacre.org>
2020-05-27 11:09:04 +0000

    include GOPATH

--- install/go.sh
+++ install/go.sh
@@ -4,6 +4,7 @@ set -e
 
 sudo wget -q -c https://dl.google.com/go/go1.13.11.linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local
 echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee /etc/profile.d/gopath.sh
+echo 'export GOPATH=$HOME/go' | sudo tee -a /etc/profile.d/gopath.sh
 mkdir -p ~/go
 
 sudo apt-get -y install build-essential
