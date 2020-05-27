5379e54f82f250faa583d92a93d7fe5d7582af26
Caleb Crane <github@simulacre.org>
2020-05-27 08:53:21 +0000

    gcloud book

---
+++ install/google-cloud-sdk.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ ! -f "/etc/apt/sources.list.d/google-cloud-sdk.list" ]; then
+  echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+fi
+which gpg || sudo apt-get -y install apt-transport-https ca-certificates gnupg
+[[ -f "/usr/share/keyrings/cloud.google.gpg" ]]  || curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+if [ which gcloud ]; then
+  sudo apt-get update && sudo apt-get -y install google-cloud-sdk 
+fi
